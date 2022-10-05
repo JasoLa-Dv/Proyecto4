e1ad1425ce8131ad88eaa11c6e340405a7fb1345
juan silva <juan_silva_o@hotmail.com>
2022-10-05 16:42:06 -0300

    subir a git

---
+++ src/views/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Footer  from '../components/Footer';
+import Resena from '../components/Resena'
+
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+            <Resena />
+            <Footer />
+        </div>
+    )
+} 
+
+export default Home;
